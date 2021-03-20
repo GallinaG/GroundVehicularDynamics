91507e5392a0c63de4b4caf064dbf28af9814052
GallinaG <73530674+GallinaG@users.noreply.github.com>
2021-03-20 17:02:24 +0100

    Update main1.m

--- My files/main1.m
+++ My files/main1.m
@@ -9,14 +9,16 @@ close all
 %% PARAMETERS AND VARIABLES
 
 syms t real %time declaration
-syms phy(t) theta(t) psi(t) %unknown functions
+syms phi(t) theta(t) psi(t) %unknown functions
 syms x(t)   y(t)    z(t)    %unknown functions
 syms s %dummy variable
 
 tstep=1000;     %change it to imporve/relax risolution
-%RPM = 3000;    % so I'll need 0.02s for complete each rampage
-t=linspace(0,0.02,tstep);
-phy = linspace(0,2*pi,tstep);
+RPM = 3000;    % so I'll need 0.02s for complete each rampage
+d_dphi=RPM/60;   %[Hz] since I'm going at 3000rpm. I impose constant the angular speed of my crank
+
+tsim=linspace(0,0.02,tstep);
+phi = linspace(0,2*pi,tstep);
 
 %% Parameters
 
@@ -28,16 +30,20 @@ m3= 1.25;   %Slider [kg]
 %lengths
 l1=0.03;     %Crank  [m]
 l2=0.12;     %Rod    [m]
-l3=(l1*cos(phy)+sqrt(((l1*cos(phy)).^2)-(l1^2-l2)))/(l1^2-l2);
+l3=(l1*cos(phi)+sqrt(((l1*cos(phi)).^2)-(l1^2-l2)))/(l1^2-l2);
 %intertial moments
-m1=0.000072;     %Crank  [kg*m2]
-m2=0.0000624;    %Rod    [kg*m2]
-m3= 0.00068;     %Slider [kg*m2]
+Im1=0.000072;     %Crank  [kg*m2]
+Im2=0.0000624;    %Rod    [kg*m2]
+Im3= 0.00068;     %Slider [kg*m2]
 
+%Inertial
+J1=(1/3)*m1*l1^2;
+J2=(1/3)*m2*l2^2;
 %% joint variables vector
-P1=[0;0;0];
-P2=[l1*cos(phy);l2*sin(phy);0];
-P3=[l3;0;0];
+P1=[zeros(1,tstep);zeros(1,tstep);zeros(1,tstep)];
+P2=[l1*cos(phi);l2*sin(phi);zeros(1,tstep)];
+P3=[l3;zeros(1,tstep);zeros(1,tstep)];
+
 %rotation matrices
 R1(s)=[ cos(s) sin(s) 0     %yaw
        -sin(s) cos(s) 0
@@ -52,4 +58,5 @@ R3(s)=[ 1   0   0           %roll
         0 -sin(s) cos(s)];
     
 
-%Inetrial
+%% Kinetic energy
+K1=(1/2)*J1*d_dpsy(t);
